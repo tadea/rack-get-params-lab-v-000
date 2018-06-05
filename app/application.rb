c1c5856efe36f9f54209f9b255664e0699567ec3
Tadea Simunovic <tsimunoviczg@gmail.com>
2018-06-05 21:23:53 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,17 +11,17 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-    elsif req.path.match(/cart/)
-      @@cart == []
-      resp.write "Your cart is empty"
-    else
-      @@cart.each do |item|
-        resp.write "#{item}\n"
-      end
-    end
+      elsif req.path.match(/search/)
+        search_term = req.params["q"]
+        resp.write handle_search(search_term)
+      elsif req.path.match(/cart/)
+        if @@cart == []
+          resp.write "Your cart is empty"
+        else
+          @@cart.each do |item|
+            resp.write "#{item}\n"
+          end
+        end
 
   elsif req.path.match(/add/)
       if @@items.include?(req.params{"item"})
